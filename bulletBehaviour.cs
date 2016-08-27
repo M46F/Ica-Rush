69595c867401d3dcf3b6eaae88ec2aedefdd934d
firas atha <vrasath@gmail.com>
2016-08-27 15:50:40 +0700

    temp turret

---
+++ bulletBehaviour.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bulletBehaviour : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector2 (transform.position.x, transform.parent.position.y);	
+		Debug.Log (transform.position);
+	}
+}
---
+++ turretBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class turretBehaviour : MonoBehaviour {
+	public Rigidbody2D projectile;
+	void Shoot(){
+		Vector3 pos = transform.position;
+		Rigidbody2D instance = (Rigidbody2D)Instantiate(projectile,transform.position,transform.rotation);
+		instance.transform.parent = transform;
+		instance.velocity = new Vector2 (1, 0);
+	}
+	void Update(){
+		Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D> ();
+		rb.velocity=new Vector2(0,-1);
+	}
+	void Awake() {
+		//InvokeRepeating("Shoot", 0, 0.3F);
+		Shoot();
+	}
+}
