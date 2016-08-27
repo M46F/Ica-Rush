19d2002ae9de918fc287dbc3f40f11b0b8536f02
Muhammad Assagaf <muhammadassagaf46@gmail.com>
2016-08-27 17:22:24 +0700

    player behav = point hp dan mati klo collision dg object bertag enemy/bullet

---
+++ PlayerBehaviourScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviourScript : MonoBehaviour {
+
+	public float initHP = 5;
+	private float point = 0;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (this.initHP <= 0) {
+			Death ();
+		}
+
+		point += Time.deltaTime;
+		Debug.Log (point);
+	}
+
+	void OncollisionEnter(Collision collision){
+		if (collision.gameObject.tag.Equals ("Enemy") || collision.gameObject.tag.Equals ("Bullet")) {
+			initHP -= 1;
+			Debug.Log ("HP sekarang : " + initHP);
+		}
+	}
+
+	void Death(){
+		Destroy (this);
+	}
+
+}
