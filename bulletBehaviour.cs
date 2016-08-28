d82c24f33f1a245d4314d08aa0b2875609ed0ba7
firas atha <vrasath@gmail.com>
2016-08-28 15:59:33 +0700

    yay

--- bulletBehaviour.cs
+++ bulletBehaviour.cs
@@ -2,10 +2,14 @@
 using System.Collections;
 
 public class bulletBehaviour : MonoBehaviour {
-
-	// Update is called once per frame
+	float lastY;
+	void Awake(){
+		lastY = transform.position.y;
+	}
 	void Update () {
-		transform.position = new Vector2 (transform.position.x, transform.parent.position.y);	
+		
+		transform.position = new Vector2 (transform.position.x, transform.position.y - (lastY - transform.parent.position.y));
+		lastY = transform.parent.position.y;
 		Debug.Log (transform.position);
 	}
 }
--- turretBehaviour.cs
+++ turretBehaviour.cs
@@ -3,18 +3,22 @@ using System.Collections;
 
 public class turretBehaviour : MonoBehaviour {
 	public Rigidbody2D projectile;
+	public float speed;
+	public Transform player;
+	public float downSpeed;
+	public float delay;
 	void Shoot(){
 		Vector3 pos = transform.position;
 		Rigidbody2D instance = (Rigidbody2D)Instantiate(projectile,transform.position,transform.rotation);
 		instance.transform.parent = transform;
-		instance.velocity = new Vector2 (1, 0);
+		Vector2 arah = player.position - transform.position;
+		instance.velocity = arah.normalized*speed;
 	}
 	void Update(){
 		Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D> ();
-		rb.velocity=new Vector2(0,-1);
+		rb.velocity=new Vector2(0,-1)*downSpeed;
 	}
 	void Awake() {
-		//InvokeRepeating("Shoot", 0, 0.3F);
-		Shoot();
+		InvokeRepeating("Shoot", 0, delay);
 	}
 }
