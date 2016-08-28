c16e7c76291d1f873ecd5b7c1b1dc5f3f32f0a5b
Malik Ariefa <Malik.Ariefa@gmail.com>
2016-08-28 19:05:07 +0700

    Fix animation

---
+++ CameraFix.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFix : MonoBehaviour {
+    public static float scale = 1f;
+    public static float pixelToUnits = 1f;
+
+    public Vector2 nativeRes = new Vector2(240,160);
+
+	void Awake () {
+        var camera = GetComponent<Camera>();
+        if(camera.orthographic){
+            scale = Screen.height / nativeRes.y;
+            pixelToUnits *= scale;
+
+            camera.orthographicSize = (Screen.height / 2.0f) / pixelToUnits;
+        }
+	}
+	
+
+}
